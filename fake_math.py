416ded43d6af11792f595399de2e008af55ba405
Trutlez <orppo@yandex.ru>
2024-12-24 15:10:56 +0500

    Модули и пакеты

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(v1, v2):
+    if (v2 == 0):
+        return 'Ошибка'
+    else:
+        return v1/v2
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(v1, v2):
+    if (v2 == 0):
+        return inf
+    else:
+        return v1/v2
